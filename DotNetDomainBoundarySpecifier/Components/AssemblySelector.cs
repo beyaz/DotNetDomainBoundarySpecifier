d1e8182c8b52e107d668e7e8e7a070fdfd18e530
beyaz <beyaz1404@gmail.com>
2024-05-18 15:31:13 +0300

    .

---
+++ DotNetDomainBoundarySpecifier/Components/AssemblySelector.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiInspector.WebUI.Components;
+
+sealed class AssemblySelector : Component
+{
+    protected override Element render()
+    {
+        return new ItemList<string>
+        {
+            Records = Directory.GetFiles(Config.AssemblySearchDirectory, "*.dll").Where(x => !isInDomain(x)).ToList(),
+        };
+    }
+
+}
---
+++ DotNetDomainBoundarySpecifier/Components/List.cs
@@ -0,0 +1,53 @@
+﻿namespace ApiInspector.WebUI.Components;
+
+sealed class ItemList<TRecord> : Component<ItemList<TRecord>.State>
+{
+    public required IReadOnlyList<TRecord> Records { get; init; }
+    
+    public IReadOnlyList<int> SelectedRecordIndexList { get; init; }
+    
+    protected override Element render()
+    {
+        return new FlexColumn
+        {
+            new FlexRowCentered(Padding(8,16),Background("#f9fafb"))
+            {
+                new input
+                {
+                    type                     = "text",
+                    placeholder              = "Search...",
+                    valueBind                = () => state.SearchText,
+                    valueBindDebounceTimeout = 700,
+                    valueBindDebounceHandler = OnSearchKeyPressFinished,
+                    style =
+                    {
+                        WidthFull
+                    }
+                }
+            },
+            
+            new FlexColumn(AlignItemsCenter,Gap(4))
+            {
+                Records.Select(ToElement)
+            }
+        };
+    }
+    
+    Element ToElement(TRecord record)
+    {
+        return new FlexRowCentered(Border(1, solid, Theme.BorderColor), BorderRadius(4), Padding(4,8), CursorDefault, Hover(Border(1,solid,"blue")))
+        {
+            record.ToString()
+        };
+    }
+
+    Task OnSearchKeyPressFinished()
+    {
+        return Task.CompletedTask;
+    }
+
+    internal class State
+    {
+        public string SearchText { get; init; }
+    }
+}
