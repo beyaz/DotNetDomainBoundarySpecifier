9d63ed98682f7afeae9f6f117c009cd296599753
beyaz <beyaz1404@gmail.com>
2024-05-21 21:33:58 +0300

    .

--- DotNetDomainBoundarySpecifier/DotNetDependencyExtractor/Models.cs
+++ DotNetDomainBoundarySpecifier/DotNetDependencyExtractor/Models.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Immutable;
-
-namespace DotNetDependencyExtractor;
+﻿namespace DotNetDependencyExtractor;
 
 sealed record AssemblyAnalyse
 {
@@ -11,26 +9,6 @@ sealed record AssemblyAnalyse
     public IReadOnlyList<MethodReference> CalledMethods { get; init; }
 }
 
-sealed record TypeExportInfo
-{
-    public string ClassName { get; init; }
-
-    public bool ExportAsEnum { get; init; }
-
-    public TypeDefinition TypeDefinition { get; init; }
-
-    public IReadOnlyList<PropertyDefinition> UsedProperties { get; init; }
-}
-
-sealed record TypeExportContext
-{
-    public ImmutableList<TypeExportInfo> ExportList { get; init; }
-
-    public IReadOnlyList<AssemblyAnalyse> CardOrchestrations { get; init; }
-    
-    public IReadOnlyList<AssemblyAnalyse> CardSystemSearchFiles { get; init; }
-}
-
 sealed record FileModel
 {
     public string Name { get; init; }
@@ -38,23 +16,6 @@ sealed record FileModel
     public string Content { get; init; }
 
     public string DirectoryPath { get; init; }
-} 
-
-sealed record GenerateDependentCodeInput
-{
-    public string TargetAssembly { get; init; }
-
-    public string TargetMethodName { get; init; }
-
-    public string TargetTypeFullName { get; init; }
-
-    internal AssemblyDefinition TargetAssemblyDefinition { get; init; }
-
-    internal MethodDefinition TargetMethod { get; init; }
-
-    internal TypeDefinition TargetType { get; init; }
-    
-    internal IReadOnlyList<AssemblyAnalyse> CardSystemSearchFiles { get; init; }
 }
 
 sealed record GenerateDependentCodeOutput
