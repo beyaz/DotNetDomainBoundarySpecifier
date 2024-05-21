2eeeb3fffb1d13a8008c6d5dc5c37278fcc08562
beyaz <beyaz1404@gmail.com>
2024-05-21 22:00:48 +0300

    .

--- DotNetDomainBoundarySpecifier/GlobalUsings.cs
+++ DotNetDomainBoundarySpecifier/GlobalUsings.cs
@@ -10,3 +10,10 @@ global using static DotNetDependencyExtractor.Extractor;
 global using System.Collections.Immutable;
 global using DotNetDomainBoundarySpecifier.Utilities;
 global using DotNetDomainBoundarySpecifier.Process;
+
+global using System;
+global using System.IO;
+global using Mono.Cecil;
+global using Mono.Cecil.Cil;
+global using static DotNetDependencyExtractor.FileHelper;
+global using static DotNetDependencyExtractor.CecilHelper;
--- DotNetDomainBoundarySpecifier/Process/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using System;
-global using System.IO;
-global using Mono.Cecil;
-global using Mono.Cecil.Cil;
-global using static DotNetDependencyExtractor.FileHelper;
-global using static DotNetDependencyExtractor.CecilHelper;
