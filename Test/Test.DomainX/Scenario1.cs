566710cdb9cdf42a863845275d2e6b7e91a04e4b
beyaz <beyaz1404@gmail.com>
2024-05-23 07:55:26 +0300

    .

---
+++ Test/Test.DomainX/Scenario1.cs
@@ -0,0 +1,11 @@
+﻿namespace Test.DomainX;
+
+public class Scenario1
+{
+    public static void AnyMethod()
+    {
+        var process = new ExternalDomainY.Scenario1.Process1();
+
+        process.Method1(default,default,default);
+    }
+}
--- Test/Test.ExternalDomainY/UnitTest1.cs
+++ Test/Test.ExternalDomainY/UnitTest1.cs
@@ -1,4 +1,5 @@
 using DotNetDomainBoundarySpecifier.Processors;
+using FluentAssertions;
 
 namespace DotNetDomainBoundarySpecifier.Tests;
 
@@ -8,11 +9,23 @@ public class Scenario1
     [TestMethod]
     public void Scenario1Test()
     {
-        Analyzer.AnalyzeMethod(new(), new()
+        var analyzeMethodInput = new Analyzer.AnalyzeMethodInput()
         {
-            AssemblyFileName = "Test.DomainX",
-            TypeFullName     = "Test.ExternalDomainY.Scenario1",
-            MethodFullName   = "gg"
-        });
+            AssemblyFileName = "Test.ExternalDomainY.dll",
+            TypeFullName     = "Test.ExternalDomainY.Scenario1.Process1",
+            MethodFullName   = "System.String Test.ExternalDomainY.Scenario1.Process1::Method1(System.String,System.Int32,System.Nullable`1<System.DateTime>)"
+        };
+        
+        var records = Analyzer.AnalyzeMethod(new(), analyzeMethodInput);
+
+        records.Count.Should().Be(0);
+
+        var generationOutput = Analyzer.GenerateCode(new(), analyzeMethodInput, records);
+
+        var expected =
+            """
+
+           """;
+        generationOutput.ContractFile.Content.Trim().Should().BeEquivalentTo(expected);
     }
 }
