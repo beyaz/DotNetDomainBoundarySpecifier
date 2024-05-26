1477ff2840ed414ca5a7e95eaa77f907d04a9a6d
beyaz <beyaz1404@gmail.com>
2024-05-26 19:29:22 +0300

    .

--- Test/Test.ExternalDomainY/GenerationTest.cs
+++ Test/Test.ExternalDomainY/GenerationTest.cs
@@ -241,4 +241,102 @@ public class GenerationTest
             """;
         generationOutput.ContractFile.Content.Trim().Should().BeEquivalentTo(expected.Trim());
     }
+    
+    [TestMethod]
+    public void Method6()
+    {
+        var analyzeMethodInput = new Analyzer.AnalyzeMethodInput()
+        {
+            AssemblyFileName = "Test.ExternalDomainY.dll",
+            TypeFullName     = "Test.ExternalDomainY.AnyProcess",
+            MethodFullName   = "Test.ExternalDomainY.GenericResponse`1<System.Int32> Test.ExternalDomainY.AnyProcess::Method6(Test.ExternalDomainY.A,Test.ExternalDomainY.ObjectHelper)"
+        };
+        
+        var methodBoundary = Analyzer.AnalyzeMethod(DefaultScope, analyzeMethodInput);
+
+        methodBoundary.Properties.Count.Should().Be(9);
+
+        var generationOutput = Analyzer.GenerateCode(DefaultScope, analyzeMethodInput, methodBoundary);
+
+        var expected =
+            """
+            namespace _Contracts_.Test.ExternalDomainY.AnyProcess.Method6;
+
+            using Output = int;
+
+            public sealed class Method6Input : IBankingProxyInput<Output>
+            {
+                public string Property0 { get; set; }
+                public int Property1 { get; set; }
+                public DateTime? Property2 { get; set; }
+                public long Property3 { get; set; }
+                public B Property5 { get; set; }
+            }
+
+            public sealed class B
+            {
+                public C Nested { get; set; }
+                public string X { get; set; }
+                public int Y { get; set; }
+            }
+
+            public sealed class C
+            {
+                public DateTime? Z { get; set; }
+            }
+            """;
+        generationOutput.ContractFile.Content.Trim().Should().BeEquivalentTo(expected.Trim());
+    }
+    
+    [TestMethod]
+    public void Method7()
+    {
+        var analyzeMethodInput = new Analyzer.AnalyzeMethodInput()
+        {
+            AssemblyFileName = "Test.ExternalDomainY.dll",
+            TypeFullName     = "Test.ExternalDomainY.AnyProcess",
+            MethodFullName   = "Test.ExternalDomainY.GenericResponse`1<Test.ExternalDomainY.AnyOutput> Test.ExternalDomainY.AnyProcess::Method7(Test.ExternalDomainY.ObjectHelper,Test.ExternalDomainY.A)"
+        };
+        
+        var methodBoundary = Analyzer.AnalyzeMethod(DefaultScope, analyzeMethodInput);
+
+        methodBoundary.Properties.Count.Should().Be(11);
+
+        var generationOutput = Analyzer.GenerateCode(DefaultScope, analyzeMethodInput, methodBoundary);
+
+        var expected =
+            """
+            namespace _Contracts_.Test.ExternalDomainY.AnyProcess.Method7;
+            
+            using Output = AnyOutput;
+            
+            public sealed class Method7Input : IBankingProxyInput<Output>
+            {
+                public string Property0 { get; set; }
+                public int Property1 { get; set; }
+                public DateTime? Property2 { get; set; }
+                public long Property3 { get; set; }
+                public B Property5 { get; set; }
+            }
+            
+            public sealed class AnyOutput
+            {
+                public int OutputProperty1 { get; set; }
+                public long OutputProperty3 { get; set; }
+            }
+            
+            public sealed class B
+            {
+                public C Nested { get; set; }
+                public string X { get; set; }
+                public int Y { get; set; }
+            }
+            
+            public sealed class C
+            {
+                public DateTime? Z { get; set; }
+            }
+            """;
+        generationOutput.ContractFile.Content.Trim().Should().BeEquivalentTo(expected.Trim());
+    }
 }
